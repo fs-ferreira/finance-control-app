57e3a71606ae66cf166d158c3ad7a20a7afc842a
Felipe Ferreira <felipeferreira0811@gmail.com>
2024-01-23 17:25:17 -0300

    23/01/24 - Commit inicial com imports e incialização de rotas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) },
+  // { path: '', redirectTo: '/reports', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
